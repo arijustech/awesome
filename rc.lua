a05404411991af234442ec60adbd00de67de1e69
arijustech <arijus.ka@outlook.com>
2025-05-13 20:16:22 +0300

    update

--- rc.lua
+++ rc.lua
@@ -316,7 +316,9 @@ clientkeys = gears.table.join(
               {description = "move to master", group = "client"}),
     awful.key({ modkey,           }, "o",      function (c) c:move_to_screen()               end,
               {description = "move to screen", group = "client"}),
-    awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
+    awful.key({ modkey }, "t", function () awful.layout.set(awful.layout.suit.tile) end,
+            {description = "set tiling layout", group = "layout"}),
+    awful.key({ modkey,           }, "p",      function (c) c.ontop = not c.ontop            end,
               {description = "toggle keep on top", group = "client"}),
     awful.key({ modkey,           }, "n",
         function (c)
